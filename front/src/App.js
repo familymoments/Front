c5f6ba43a5a06998f4833527d9040b468fcf4234
SooY2 <ssy67366736@gmail.com>
2023-07-16 06:14:17 +0900

    [fecth] 레이아웃설정

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,27 @@
 // import "./App.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import classes from "./App.module.css";
-import WhiteSpace from "./components/WhiteSpace";
-import NewFamily from "./pages/NewFamily";
+import Landing from './pages/Landing';
 
 
 function App() {
+    function setScreenSize() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+      }
+      useEffect(() => {
+        setScreenSize();
+      });
     return (
         <div className={classes.App}>
-            <WhiteSpace />
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<NewFamily />} />
-                    <Route path="/example" element={<></>} />
+                    <Route path="/" element={<Landing/>} />
                 </Routes>
             </BrowserRouter>
-            <WhiteSpace />
+
+            
         </div>
     );
 }
---
+++ front/src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+const Landing = ()=>{
+    return <div>
+        <Header title="Family Moments"></Header>
+    </div>
+}
+
+export default Landing;
