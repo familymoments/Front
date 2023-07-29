a1a7d91dc9fb6facf9c9c6388a09121441da9229
arthur <iii1003@naver.com>
2023-07-29 19:39:18 +0900

    [refactor] 로그인 화면 퍼블리싱

--- front/src/pages/Findid/index.jsx
+++ front/src/pages/Findid/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link, Switch } from 'react-router-dom';
+
 import Header from '../../components/Header';
 import Loginbutton from '../../components/Loginbutton';
 import Styles from './Findid.module.css'
@@ -12,9 +12,9 @@ function Findid(){
         <div className={Styles.top}>
         <h1 id = {Styles.toptext}>아이디 찾기</h1>
         <p id = {Styles.topsmalltext}>본인 확인을 위하여 이메일로 인증해주세요</p>
-        <h2 id ={Styles.bottom}>이름</h2>
+        <h2 className ={Styles.topname}>이름</h2>
         </div>
-        <form>
+        <form className={Styles.nameform}>
         
         <Inputwindow placeholder='실명을 입력하세요 ex) 홍길동'></Inputwindow>
         <button className = {Styles.deletebutton} type = "submit"><TiDeleteOutline/></button> 
@@ -22,15 +22,17 @@ function Findid(){
 
 
         <h2 id ={Styles.bottom}>이메일</h2>
-        <div>
-        <Inputwindow placeholder = 'example@email.com'></Inputwindow>
-        
-        <button></button>
-        </div>
+        <div className={Styles.confirmemail}>
+            <form>
+            <Inputwindow placeholder = 'example@email.com'></Inputwindow>
+            <button></button>
+        </form>
         
         <Inputwindow placeholder = '인증번호 6자리'></Inputwindow>
-        <Loginbutton texts = "순간을 가족에게 공유하기"/>
-       
+        </div>
+        <div id = {Styles.findidbutton}>
+        <Loginbutton  texts = "순간을 가족에게 공유하기"/>
+        </div>
         </div>);
 }
 
