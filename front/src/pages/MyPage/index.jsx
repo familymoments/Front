ee26f087e44224667126e2fbe35a6e8fee776a14
g00hyun <rhkdgus1417@hufs.ac.kr>
2023-07-30 22:53:46 +0900

    [feat] 마이페이지

--- front/src/pages/MyPage/index.jsx
+++ front/src/pages/MyPage/index.jsx
@@ -1,9 +1,23 @@
 import classes from "./MyPage.module.css";
+import Nav from "../../components/Nav";
+import NavAttr from "../../components/NavAttr";
+import { FiUser, FiLock, FiBell, FiSettings, FiStar } from "react-icons/fi";
+import { TbDoorExit } from "react-icons/tb";
 
+// 마이페이지
 const MyPage = () => {
     return (
-        <div>
-            마이페이지
+        <div className={classes.wrapper}>
+            <Nav style={classes.account} title="Account">
+                <NavAttr iconName={FiUser} title="Profile" lnk="./profile" />
+                <NavAttr iconName={FiLock} title="Password" lnk="./pw" />
+                <NavAttr iconName={FiBell} title="알림" lnk="./notice" />
+                <NavAttr iconName={FiSettings} title="가족 설정" lnk="./setting" />
+            </Nav>
+            <Nav title="More">
+                <NavAttr iconName={FiStar} title="Log out" />
+                <NavAttr iconName={TbDoorExit} title="계정 탈퇴" />
+            </Nav>
         </div>
     );
 };
