501ee44e032324bc256811b3ba420cd6a769aaef
g00hyun <rhkdgus1417@hufs.ac.kr>
2023-08-11 20:24:59 +0900

    [feat] album 페이지 && modal 작업

--- front/src/pages/Album/index.jsx
+++ front/src/pages/Album/index.jsx
@@ -3,6 +3,7 @@ import ImgButton from "../../components/ImgButton";
 import classes from "./Album.module.css";
 
 import axios from "axios";
+import useAsync from "../../hooks/useAsync";
 
 const authToken = localStorage.getItem("jwtToken");
 // console.log(authToken)
@@ -13,41 +14,54 @@ const headers = {
 
 const Album = () => {
     const [imgLst, setImgLst] = useState([]);
+    const [isModal, setIsModal] = useState(false);
+    const [focusImg, setFocusImg] = useState('');
 
-    // 아직 백api 구현 안됌
-    // const fetchAlbum = async () => {
-    //     const response = await axios.get(`/posts/album?familyId=1`, { headers });
-    //     console.log(response)
-    //     return response
-    // }
+    // imgButton 컴포넌트(하위 컴포넌트)에서 사용하는 함수 -> 매개변수도 imgButton 컴포넌트에서 가져옴
+    const modalHandler = (selectedImg) => {
+        setFocusImg(selectedImg)
+
+        setIsModal(true);
+    };
+
+    const getImgs = async () => {
+        const response = await axios.get(`/posts/album?familyId=5`);
+        return response.data;
+    };
+
+    const [state, refetch] = useAsync(getImgs, []);
+
+    const { loading, data, error } = state;
+
+    const albums = data?.result;
 
     useEffect(() => {
-        // fetchAlbum()
-        setImgLst([
-            {
-                id: 1,
-                lnk: "https://images.pexels.com/photos/7473282/pexels-photo-7473282.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            },
-            {
-                id: 2,
-                lnk: "https://images.pexels.com/photos/2604639/pexels-photo-2604639.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            },
-            {
-                id: 3,
-                lnk: "https://images.pexels.com/photos/2957862/pexels-photo-2957862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            },
-            {
-                id: 4,
-                lnk: "https://images.pexels.com/photos/8264573/pexels-photo-8264573.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            },
-            ...imgLst,
-        ]);
-    }, []);
+        if (Array.isArray(albums)) {
+            setImgLst([...albums]);
+        }
+        console.log(imgLst);
+    }, [albums]);
 
-    // console.log(imgLst)
+    if (loading) return <p>loading...</p>;
+    if (error) return <p>error</p>;
+    if (!albums) return <p>No exist data..</p>;
 
     return (
         <div className={classes.wrapper}>
+            {isModal && (
+                <div
+                    className={`${classes.backdrop}`}
+                    onClick={() => setIsModal(false)}
+                ></div>
+            )}
+            {isModal && (
+                <img
+                    src={focusImg}
+                    className={`${classes.modal} ${
+                        isModal ? classes.show : ""
+                    }`}
+                ></img>
+            )}
             <hr className={classes.hr} />
             <div className={classes.dotWrapper}>
                 <div className={classes.dot}></div>
@@ -58,7 +72,13 @@ const Album = () => {
             </div>
             <div className={classes.bodyWrapper}>
                 {imgLst.map((item, idx) => {
-                    return <ImgButton key={idx} {...item} />;
+                    return (
+                        <ImgButton
+                            key={idx}
+                            {...item}
+                            modalValue={modalHandler}
+                        />
+                    );
                 })}
             </div>
         </div>
