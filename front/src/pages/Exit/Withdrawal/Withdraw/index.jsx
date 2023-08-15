5e6c4989c161919e0129007a2ad3f4177770dddc
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2023-08-15 17:04:25 +0900

    [feat] 강퇴페이지

--- front/src/pages/Exit/Withdrawal/Withdraw/index.jsx
+++ front/src/pages/Exit/Withdrawal/Withdraw/index.jsx
@@ -1,11 +1,10 @@
-import Button from "../../../components/Button";
-import MyText from "../../../components/MyText";
-import classes from "../Withdrawal/Withdrawal.module.css";
-
+import Button from "../../../../components/Button";
+import MyText from "../../../../components/MyText";
+import classes from "../../SetUp/Setting/Setting.module.css";
 import{useNavigate} from "react-router-dom"
 
 
-const Withdrawal4 = () => {
+const Withdraw = () => {
     const navigate = useNavigate();
     return (
         <div>
@@ -25,15 +24,17 @@ const Withdrawal4 = () => {
             </div>
             </div>            
             <Button 
-                onClick={()=>{navigate()}}
+                onClick={()=>{navigate("/Main/withdraw2")}}
                 btn={classes.btn3} 
+                title="계속하기"
             />
             <Button 
                 onClick={()=>{navigate()}}
                 btn={classes.btn} 
+                title="취소"
                 />
         </div>
     );
 }
 
-export default Withdrawal4;
+export default Withdraw;
