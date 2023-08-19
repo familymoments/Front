5e7cbae4ed83f878384ea7f542898c869a88b89c
SooY2 <ssy67366736@gmail.com>
2023-08-19 20:44:17 +0900

    [feat] 환경변수 설정

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -16,21 +16,21 @@ module.exports = (app) => {
 
     app.use(
         createProxyMiddleware("/users", {
-            target: "http://43.202.90.230",
+            target: "process.env.REACT_APP_SERVER_URL",
             changeOrigin: true,
         })
     );
     
     app.use(
         createProxyMiddleware("/families", {
-            target: "http://43.202.90.230",
+            target: "process.env.REACT_APP_SERVER_URL",
             changeOrigin: true,
         })
     );
 
     app.use(
         createProxyMiddleware("/comments", {
-            target: "http://43.202.90.230",
+            target: "process.env.REACT_APP_SERVER_URL",
             changeOrigin: true,
         })
     );
