516377dc88558c03b87abc31163db7b4d5bbaf75
g00hyun <rhkdgus1417@hufs.ac.kr>
2023-08-20 22:39:40 +0900

    [feat] 계정 탈퇴 페이지

---
+++ front/src/pages/MyPage/Resign/Page/Resign2.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import Button from "../../../../components/Button";
+import classes from "./Resign2.module.css";
+import { useRecoilState } from "recoil";
+import { header } from "../../../../atom";
+import axios from "axios";
+
+const Resign2 = ({ idxHandler }) => {
+    const SERVER = process.env.REACT_APP_SERVER_URL;
+    const [headers, setHeaders] = useRecoilState(header);
+
+    const [password, setPassword] = useState("");
+
+    const GetPwdReq = { "password": "aaassss" };
+
+    const fetchPW = async () => {
+        const response = await axios.get(`${SERVER}/users/auth/compare-pwd`, {
+            headers,
+            GetPwdReq,
+        });
+        console.log(response.data);
+        return response.data;
+    };
+
+    const handleInputChange = (event) => {
+        const { value } = event.target;
+        setPassword(value);
+    };
+
+    const continueHandler = () => {
+        console.log("password : ", password);
+        // fetchPW();
+        alert("api 연동 필요");
+        idxHandler(2);
+    };
+
+    // useEffect(() => console.log(pw), [pw])
+
+    return (
+        <div className={classes.wrapper}>
+            <div className={classes.div}>
+                <p className={classes.p}>
+                    계속 진행하려면 보안을 위해
+                    <br />
+                    현재 비밀번호를 입력해주세요.
+                </p>
+                <input
+                    className={classes.input}
+                    placeholder="현재 비밀번호"
+                    name="password"
+                    value={password}
+                    onChange={handleInputChange}
+                ></input>
+                <Button
+                    title="계속하기"
+                    btn={classes.btn}
+                    onClick={continueHandler}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Resign2;
