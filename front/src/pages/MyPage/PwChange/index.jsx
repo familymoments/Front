0c4fe7e0639e9c2241cb4c9cdd6b575c3be718e8
g00hyun <rhkdgus1417@hufs.ac.kr>
2023-08-21 04:34:37 +0900

    [feat] 비밀번호 변경 페이지 에러 처리

--- front/src/pages/MyPage/PwChange/index.jsx
+++ front/src/pages/MyPage/PwChange/index.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import Button from "../../../components/Button";
 import classes from "./PwChange.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { header } from "../../../atom";
+import { useNavigate } from "react-router-dom";
 
 const authToken = localStorage.getItem("token");
 
@@ -13,7 +14,7 @@ const headers = {
 
 const PwChange = () => {
     // 응답코드 바뀐거 확인되면 맞춰서 오류 메세지 띄우기
-    const [code, setCode] = useState(200);
+    const [code, setCode] = useState(0);
     const [message, setMessage] = useState("");
     const [data, setData] = useState({
         password: "",
@@ -23,6 +24,8 @@ const PwChange = () => {
 
     const SERVER = process.env.REACT_APP_SERVER_URL;
     const [headers, setHeaders] = useRecoilState(header);
+
+    const nav = useNavigate();
 
     const handleInputChange = (event) => {
         const { name, value } = event.target;
@@ -44,9 +47,12 @@ const PwChange = () => {
         // return response.data;
     };
 
-    // const testF = () => {
-    //     console.log('helle')
-    // }
+    useEffect(() => {
+        if (code === 200) {
+            localStorage.removeItem("token");
+            nav("/");
+        }
+    }, [code])
 
     return (
         <div className={classes.wrapper}>
@@ -68,22 +74,28 @@ const PwChange = () => {
                 placeholder="현재 비밀번호"
                 name="password"
                 value={data.password}
+                type="password"
                 onChange={handleInputChange}
             ></input>
+            {code === 4000 && <p className={classes.errorText}>{message}</p>}
             <input
                 className={classes.input}
                 placeholder="새 비밀번호"
                 name="newPassword_first"
                 value={data.newPassword_first}
+                type="password"
                 onChange={handleInputChange}
             ></input>
+            {(code === 4001 || code === 4003 || code === 4004 ) && <p className={classes.errorText}>{message}</p>}
             <input
                 className={classes.input}
                 placeholder="새 비밀번호 확인"
                 name="newPassword"
                 value={data.newPassword}
+                type="password"
                 onChange={handleInputChange}
             ></input>
+            {code === 4002 && <p className={classes.errorText}>{message}</p>}
             <Button
                 title="비밀번호 변경"
                 btn={classes.btn}
