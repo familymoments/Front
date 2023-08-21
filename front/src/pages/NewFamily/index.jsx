005947a009c0ddfe0ec023aed8632e886fd1971f
g00hyun <rhkdgus1417@hufs.ac.kr>
2023-08-22 03:48:02 +0900

    [feat] main화면으로 가는 라우팅 처리

--- front/src/pages/NewFamily/index.jsx
+++ front/src/pages/NewFamily/index.jsx
@@ -1,20 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import CircleButton from "../../components/CircleButton";
 import classes from "./NewFamily.module.css";
 
 const NewFamily = () => {
+    const nav = useNavigate();
+
+    const nextHandler = () => {
+        nav("/main")
+        window.location.reload();
+    }
+
     return (
         <div className={classes.NewFamily}>
             <div className={classes.area}>
                 <Link to="../createfamily2">
-                <CircleButton title="우리 가족 생성하기" type="pink" layout={classes.pinkCircleBtn} />
+                    <CircleButton
+                        title="우리 가족 생성하기"
+                        type="pink"
+                        layout={classes.pinkCircleBtn}
+                    />
                 </Link>
                 <Link to="../familyparticipation">
-                <CircleButton title="우리 가족 참여하기" type="purple" layout={classes.purpleCircleBtn} />
+                    <CircleButton
+                        title="우리 가족 참여하기"
+                        type="purple"
+                        layout={classes.purpleCircleBtn}
+                    />
                 </Link>
             </div>
-            <Button btn={classes.btn} title="다음에 하기"/>
+                <Button btn={classes.btn} title="다음에 하기" onClick={nextHandler} />
         </div>
     );
 };
